9e2910deacd17fb2dd2133ce3d7171029997aa5c
eunhoo0903 <eunhoo0903@naver.com>
2025-01-07 20:16:01 +0900

    JWT 사용자 인증 구현

--- app/core/config.py
+++ app/core/config.py
@@ -4,3 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DB_URL = os.getenv("DB_URL")
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
+if not SECRET_KEY:
+    raise ValueError("No SECRET_KEY set for FastAPI application")
