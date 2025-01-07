e3a3e41d90fa3684867d04da67321c90489ac54b
eunhoo0903 <eunhoo0903@naver.com>
2025-01-07 21:03:03 +0900

    /login API 구현 (로그인 기능 추가)

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -3,13 +3,13 @@ from sqlalchemy.orm import Session
 from core.security import create_access_token, verify_password, get_password_hash
 from db.database import get_db
 from db.models import User
-from schemas.auth import UserCreate, UserResponse, UserLogin
+from schemas.auth import UserCreate, Token, UserResponse, UserLogin
 
 router = APIRouter()
 
 @router.post("/signup", response_model=UserResponse, tags=["Auth"])
 def signup(user: UserCreate, db: Session = Depends(get_db)):
-    existing_user = db.query(User).filter(User.email == user.email or User.username == user.username).first()
+    existing_user = db.query(User).filter(User.email == user.email).first()
     if existing_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     
@@ -19,3 +19,12 @@ def signup(user: UserCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+@router.post("/login", response_model=Token, tags=["Auth"])
+def login(user: UserLogin, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.email == user.email).first()
+    if not db_user or not verify_password(user.password, db_user.hashed_password):
+        raise HTTPException(status_code=400, detail="Invalid email or password")
+    
+    access_token = create_access_token(data={"sub": db_user.email})
+    return {"access_token": access_token, "token_type": "Bearer"}
