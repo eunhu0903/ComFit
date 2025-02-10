ec8f1e1b031edcb5c195f177f3c556d155ddf0b5
eunhoo0903 <eunhoo0903@naver.com>
2025-02-10 19:28:20 +0900

    /signup

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 from db.session import Base, engine
+from api import user
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.include_router(user.router)
 
 @app.get("/")
 def root_read():
--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP
-from sqlalchemy.sql import func
+from sqlalchemy import Column, Integer, String, Boolean
 from db.session import Base
 
 class User(Base):
@@ -8,8 +7,6 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String(10), unique=True, index=True)
     email = Column(String(100), unique=True, index=True)
-    hashed_password = Column(String(16))
+    hashed_password = Column(String(255))
     is_active = Column(Boolean, default=True)
     is_admin = Column(Boolean, default=False)
-    created_at = Column(TIMESTAMP, server_default=func.now())
-    updated_at = Column(TIMESTAMP, server_default=func.now(), onupdate=func.current_timestamp())
