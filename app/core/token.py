3667e5afd9a85e6ea787263d24759936bc5d8e1c
eunhoo0903 <eunhoo0903@naver.com>
2025-02-11 18:02:51 +0900

    Implement token verification and user authentication

---
+++ app/core/token.py
@@ -0,0 +1,39 @@
+from fastapi import HTTPException, status
+from datetime import datetime
+from sqlalchemy.orm import Session
+from models.user import User
+from security import decode_access_token
+
+def verify_token(token: str, db: Session) -> str:
+    try:
+        payload = decode_access_token(token)
+
+        email: str = payload.get("email")
+
+        if email is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Tokeb has no email",
+            )
+        
+        user = db.query(User).filter(User.email == email).first()
+        if user is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid email or token",
+            )
+        
+        expiration: int = payload.get("exp")
+        if expiration and expiration < int(datetime.utcnow().timestamp()):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Token has expired",
+            )
+
+        return email
+    
+    except ValueError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+        )
