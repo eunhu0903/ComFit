2f6e94e2668e6b699f983941f114345c3da3f3f5
eunhoo0903 <eunhoo0903@naver.com>
2025-02-12 04:12:49 +0900

    메인 페이지 커밋 랭킹 기능 구현

--- app/api/home.py
+++ app/api/home.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from sqlalchemy.sql import func
 from core.token import verify_token
 from models.user import User
+from models.commit import Commit
 from db.session import get_db
 from core.token import get_token_from_header
+
 
 router = APIRouter()
 
@@ -16,7 +19,20 @@ def home(authorization: str = Depends(get_token_from_header), db: Session = Depe
     if not user:
         raise HTTPException(status_code=401,detail="User not found")
     
-    username = user.username
-    return {"message": f"{username}님, 환영합니다!"}
+    welcome_message = f"{user.username}님, 환영합니다!"
+
+    top_committers = (
+        db.query(User.username, func.count(Commit.id).label("commit_count"))
+        .join(Commit, User.id == Commit.user_id)
+        .group_by(User.username)
+        .order_by(func.count(Commit.id).desc())
+        .limit(5)
+        .all()
+    )
+
+    return {
+        "message": welcome_message,
+        "top_committers": [{"username": u, "commit_count": c} for u, c in top_committers],
+    }
 
 
